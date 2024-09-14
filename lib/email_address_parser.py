17f32bd9633be9719c5f608a0d51bf945343ff42
Ngeny Gilbert <ngenygkip@gmail.com>
2024-09-15 00:33:05 +0300

    python-p3-email_address_parser,py

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,11 @@
-# your code goes here!
+
+import re
+
+class EmailAddressParser:
+    def __init__(self, text):
+        self.text = text
+
+    def parse(self):
+        """Extracts email addresses from the provided text."""
+        email_regex = re.compile(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}')
+        return sorted(email_regex.findall(self.text))
